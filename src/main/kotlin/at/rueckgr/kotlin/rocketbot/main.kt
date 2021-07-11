b580db06d12c313f3bfac215a17158971cc56ccd
Paul Staroch <paulchen@rueckgr.at>
2021-07-12 00:54:55 +0200

    fix wrong key name

--- src/main/kotlin/at/rueckgr/kotlin/rocketbot/main.kt
+++ src/main/kotlin/at/rueckgr/kotlin/rocketbot/main.kt
@@ -117,7 +117,7 @@ fun handleResultMessage(ignoredChannels: List<String>, data: Map<String, Any>): 
 fun handleGetRoomsResult(ignoredChannels: List<String>, data: Map<String, Any>): Array<Any> {
     val rooms: List<Map<String, Any>> = data["result"] as List<Map<String, Any>>
     return rooms.filter {
-        !ignoredChannels.contains(it["fname"])
+        !ignoredChannels.contains(it["name"])
     }.map {
         val id = it["_id"]
         SubscribeMessage(id = "subscribe-$id", name = "stream-room-messages", params = arrayOf(id, false))
