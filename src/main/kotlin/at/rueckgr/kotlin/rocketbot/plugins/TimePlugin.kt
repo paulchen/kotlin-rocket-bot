6ebe499716b009ba09769a02356d9054da661fae
Paul Staroch <paulchen@rueckgr.at>
2021-07-12 21:16:58 +0200

    allow German date format without year

--- src/main/kotlin/at/rueckgr/kotlin/rocketbot/plugins/TimePlugin.kt
+++ src/main/kotlin/at/rueckgr/kotlin/rocketbot/plugins/TimePlugin.kt
@@ -13,9 +13,9 @@ class TimePlugin : AbstractPlugin() {
         val pattern: String,
         val function: (TimePlugin, String, String) -> LocalDateTime
     ) {
-        // TODO implement date without year
         DATE_EN("""^[0-9]{4}-[0-9]{2}-[0-9]{2}$""".toRegex(), "yyyy-MM-dd", TimePlugin::parseDay),
         DATE_DE("""^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}$""".toRegex(), "d.M.yyyy", TimePlugin::parseDay),
+        DATE_DE_WITHOUT_YEAR("""^[0-9]{1,2}\.[0-9]{1,2}\.$""".toRegex(), "d.M.yyyy", TimePlugin::parseDayWithoutYear),
 
         TIME_HHMM("""^[0-9]{2}:[0-9]{2}$""".toRegex(), "HH:mm", TimePlugin::parseTime),
         TIME_HHMMSS("""^[0-9]{2}:[0-9]{2}:[0-9]{2}$""".toRegex(), "HH:mm:ss", TimePlugin::parseTime),
@@ -24,6 +24,7 @@ class TimePlugin : AbstractPlugin() {
         DATETIME_EN_SECONDS("""^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$""".toRegex(), "yyyy-MM-dd HH:mm:ss", TimePlugin::parseDateTime),
         DATETIME_DE("""^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4} [0-9]{2}:[0-9]{2}$""".toRegex(), "d.M.yyyy HH:mm", TimePlugin::parseDateTime),
         DATETIME_DE_SECONDS("""^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}$""".toRegex(), "d.M.yyyy HH:mm:ss", TimePlugin::parseDateTime),
+        DATETIME_DE_WITHOUT_YEAR("""^[0-9]{1,2}\.[0-9]{1,2}\. [0-9]{2}:[0-9]{2}$""".toRegex(), "d.M.yyyy HH:mm", TimePlugin::parseDateTimeWithoutYear),
     }
 
     override fun getCommands(): List<String> {
@@ -55,6 +56,15 @@ class TimePlugin : AbstractPlugin() {
         return LocalDateTime.of(LocalDate.parse(dateString, f), LocalTime.MIDNIGHT)
     }
 
+    private fun parseDayWithoutYear(pattern: String, dateString: String): LocalDateTime {
+        val f = DateTimeFormatter.ofPattern(pattern)
+        val localDate = LocalDate.parse(dateString + LocalDate.now().year, f)
+        if (LocalDate.now().isAfter(localDate)) {
+            return LocalDateTime.of(localDate, LocalTime.MIDNIGHT).plusYears(1)
+        }
+        return LocalDateTime.of(localDate, LocalTime.MIDNIGHT)
+    }
+
     private fun parseTime(pattern: String, dateString: String): LocalDateTime {
         val f = DateTimeFormatter.ofPattern(pattern)
         val localTime = LocalTime.parse(dateString, f)
@@ -67,5 +77,15 @@ class TimePlugin : AbstractPlugin() {
     private fun parseDateTime(pattern: String, dateString: String): LocalDateTime {
         val f = DateTimeFormatter.ofPattern(pattern)
         return LocalDateTime.parse(dateString, f)
+    }
+
+    private fun parseDateTimeWithoutYear(pattern: String, dateString: String): LocalDateTime {
+        val f = DateTimeFormatter.ofPattern(pattern)
+        val localDate = LocalDateTime.parse(dateString.replace(" ", LocalDate.now().year.toString() + " "), f)
+        if (LocalDateTime.now().isAfter(localDate)) {
+            return localDate.plusYears(1)
+        }
+        return localDate
+
     }
 }
