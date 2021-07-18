a2157c848ad11a0e1e89334c0b5522341cde4bc8
Paul Staroch <paulchen@rueckgr.at>
2021-07-18 14:19:22 +0200

    move method handleGetRoomsResult() from main.kt to where it belongs

--- src/main/kotlin/at/rueckgr/kotlin/rocketbot/handler/message/ResultMessageHandler.kt
+++ src/main/kotlin/at/rueckgr/kotlin/rocketbot/handler/message/ResultMessageHandler.kt
@@ -1,7 +1,6 @@
 package at.rueckgr.kotlin.rocketbot.handler.message
 
 import at.rueckgr.kotlin.rocketbot.BotConfiguration
-import at.rueckgr.kotlin.rocketbot.handleGetRoomsResult
 import at.rueckgr.kotlin.rocketbot.webservice.RoomsGetMessage
 import at.rueckgr.kotlin.rocketbot.webservice.SubscribeMessage
 
@@ -19,4 +18,14 @@ class ResultMessageHandler : AbstractMessageHandler() {
         "get-rooms-initial" -> handleGetRoomsResult(configuration.ignoredChannels, data)
         else -> emptyArray()
     }
-}
+
+    @Suppress("UNCHECKED_CAST")
+    private fun handleGetRoomsResult(ignoredChannels: List<String>, data: Map<String, Any>): Array<Any> {
+        val rooms: List<Map<String, Any>> = data["result"] as List<Map<String, Any>>
+        return rooms.filter {
+            !ignoredChannels.contains(it["name"])
+        }.map {
+            val id = it["_id"]
+            SubscribeMessage(id = "subscribe-$id", name = "stream-room-messages", params = arrayOf(id, false))
+        }.toTypedArray()
+    }}
--- src/main/kotlin/at/rueckgr/kotlin/rocketbot/main.kt
+++ src/main/kotlin/at/rueckgr/kotlin/rocketbot/main.kt
@@ -1,7 +1,5 @@
 package at.rueckgr.kotlin.rocketbot
 
-import at.rueckgr.kotlin.rocketbot.webservice.*
-
 fun main() {
     // TODO store authentication token somewhere
     val host = System.getenv("ROCKETCHAT_HOST") ?: return
@@ -12,16 +10,4 @@ fun main() {
     Bot(BotConfiguration(host, username, password, ignoredChannels)).launch()
 
 }
-
-@Suppress("UNCHECKED_CAST")
-fun handleGetRoomsResult(ignoredChannels: List<String>, data: Map<String, Any>): Array<Any> {
-    val rooms: List<Map<String, Any>> = data["result"] as List<Map<String, Any>>
-    return rooms.filter {
-        !ignoredChannels.contains(it["name"])
-    }.map {
-        val id = it["_id"]
-        SubscribeMessage(id = "subscribe-$id", name = "stream-room-messages", params = arrayOf(id, false))
-    }.toTypedArray()
-}
-
 
