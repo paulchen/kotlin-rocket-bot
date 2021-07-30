1dd28afb140ced61ce73ea5516e77917554d7fe3
Paul Staroch <paulchen@rueckgr.at>
2021-07-31 00:09:41 +0200

    add Icinga check script

---
+++ misc/check_bot.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import requests, sys, os
+
+if 'WEBSERVICE_PORT' not in os.environ:
+    print('Environment variable WEBSERVICE_PORT not set')
+    sys.exit(3)
+
+webservice_port = os.environ['WEBSERVICE_PORT']
+
+r = requests.get('http://localhost:' + webservice_port + '/status')
+if not r:
+    print('UNKNOWN: Status code %s received from frontend' % r.status_code)
+    sys.exit(3)
+
+j = r.json()
+status = j['status']
+last_ping = j['lastPing']
+
+message = ('%s: Last ping at %s' % (status, last_ping))
+if status == 'CRITICAL':
+    return_code = 2
+elif status == 'WARNING':
+    return_code = 1
+elif status == 'OK':
+    return_code = 0
+else:
+    message = 'UNKNOWN: Unknown status %s' % (status)
+    return_code = 3
+
+print(message)
+sys.exit(return_code)
