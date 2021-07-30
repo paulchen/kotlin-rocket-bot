8bc595560fe9f442a95851381ff4699c52590b19
Paul Staroch <paulchen@rueckgr.at>
2021-07-30 23:39:42 +0200

    remember last ping time

--- src/main/kotlin/at/rueckgr/kotlin/rocketbot/handler/message/PingMessageHandler.kt
+++ src/main/kotlin/at/rueckgr/kotlin/rocketbot/handler/message/PingMessageHandler.kt
@@ -2,9 +2,17 @@ package at.rueckgr.kotlin.rocketbot.handler.message
 
 import at.rueckgr.kotlin.rocketbot.BotConfiguration
 import at.rueckgr.kotlin.rocketbot.webservice.PongMessage
+import java.time.LocalDateTime
 
 class PingMessageHandler : AbstractMessageHandler() {
+    companion object {
+        var lastPing: LocalDateTime = LocalDateTime.now()
+    }
+
     override fun getHandledMessage() = "ping"
 
-    override fun handleMessage(configuration: BotConfiguration, data: Map<String, Any>): Array<Any> = arrayOf(PongMessage())
+    override fun handleMessage(configuration: BotConfiguration, data: Map<String, Any>): Array<Any> {
+        lastPing = LocalDateTime.now()
+        return arrayOf(PongMessage())
+    }
 }
