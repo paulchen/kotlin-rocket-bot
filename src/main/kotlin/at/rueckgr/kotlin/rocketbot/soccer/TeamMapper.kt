6ff76932cc0b37e256455ebc731ba5dfa5516d91
Paul Staroch <paulchen@rueckgr.at>
2022-10-09 22:51:19 +0200

    don't hardcode all the country names and their ISO codes

--- src/main/kotlin/at/rueckgr/kotlin/rocketbot/soccer/TeamMapper.kt
+++ src/main/kotlin/at/rueckgr/kotlin/rocketbot/soccer/TeamMapper.kt
@@ -1,50 +1,25 @@
 package at.rueckgr.kotlin.rocketbot.soccer
 
+import at.rueckgr.kotlin.rocketbot.util.LocaleHelper
+
 data class FormattedTeamName (val name: String, val flag: String)
 
 object TeamMapper {
     private val flags = mapOf(
-        "Argentina" to ":ar:",
-        "Australia" to ":au:",
-        "Belgium" to ":be:",
-        "Brazil" to ":br:",
-        "Cameroon" to ":cm:",
-        "Canada" to ":cn:",
-        "Costa Rica" to ":cr:",
-        "Croatia" to ":hr:",
-        "Denmark" to ":dk:",
-        "Ecuador" to ":ec:",
         "England" to ":england:",
-        "France" to ":fr:",
-        "Germany" to ":de:",
-        "Ghana" to ":gh:",
-        "Iran" to ":ir:",
-        "Japan" to ":jp:",
-        "Mexico" to ":mx:",
-        "Morocco" to ":ma:",
-        "Netherlands" to ":nl:",
-        "Poland" to ":pl:",
-        "Portugal" to ":pt:",
-        "Qatar" to ":qa:",
-        "Saudi Arabia" to ":sa:",
-        "Senegal" to ":sn:",
-        "Serbia" to ":rs:",
-        "South Korea" to ":kr:",
-        "Spain" to ":es:",
-        "Switzerland" to ":ch:",
-        "Tunisia" to ":tn:",
-        "Uruguay" to ":uy:",
         "USA" to ":us:",
         "Wales" to ":wales:",
     )
 
     fun mapTeamName(teamName: String): FormattedTeamName {
-        val flag = if(flags.containsKey(teamName)) {
-            flags[teamName]!!
+        val locale = LocaleHelper().getLocaleByCountryName(teamName)
+        if (locale == null) {
+            if (flags.containsKey(teamName)) {
+                return FormattedTeamName(teamName, flags[teamName]!!)
+            }
+            return FormattedTeamName(teamName, "")
         }
-        else {
-            ""
-        }
-        return FormattedTeamName(teamName, flag)
+        val flag = ":" + locale.country.lowercase() + ":"
+        return FormattedTeamName(LocaleHelper().getLocalizedCountryName(locale), flag)
     }
 }
