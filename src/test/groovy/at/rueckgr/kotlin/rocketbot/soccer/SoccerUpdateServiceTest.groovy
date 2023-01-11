d988996a2c125b5cd5fb4dbab856d7e8dfe9911a
Paul Staroch <paulchen@rueckgr.at>
2023-01-11 22:36:15 +0100

    fix test

--- src/test/groovy/at/rueckgr/kotlin/rocketbot/soccer/SoccerUpdateServiceTest.groovy
+++ src/test/groovy/at/rueckgr/kotlin/rocketbot/soccer/SoccerUpdateServiceTest.groovy
@@ -1,14 +1,11 @@
 package at.rueckgr.kotlin.rocketbot.soccer
 
-import at.rueckgr.kotlin.rocketbot.Bot
 import at.rueckgr.kotlin.rocketbot.util.FixtureFactory
 import spock.lang.Specification
 
 class SoccerUpdateServiceTest extends Specification {
     def "createMessage"() {
         given:
-            Bot.knownChannelNamesToIds.put("roomName", "roomId")
-
             def service = new SoccerUpdateService()
             def fixture = FixtureFactory.createFixture("Al Bayt Stadium", "Al Khor", "Qatar", "Ecuador")
 
@@ -16,8 +13,8 @@ class SoccerUpdateServiceTest extends Specification {
             def result = service.createMessage(fixture, "roomName", "test message", "user")
 
         then:
-            result.roomId == "roomId"
-            result.roomName == null
+            result.roomId == null
+            result.roomName == "roomName"
             result.message == ":mega: :flag_qa:\u00a0*Katar*\u00a0-\u00a0:flag_ec:\u00a0*Ecuador*: test message"
             result.emoji == ":soccer:"
             result.username == "user"
