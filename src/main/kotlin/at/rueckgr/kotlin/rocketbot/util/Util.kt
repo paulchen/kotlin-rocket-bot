fddd11ebf4681b36d966fdc1488611058cab028b
Paul Staroch <paulchen@rueckgr.at>
2023-01-29 01:47:47 +0100

    properly handle Unicode surrogates and emoji ZWJ sequences

--- src/main/kotlin/at/rueckgr/kotlin/rocketbot/util/Util.kt
+++ src/main/kotlin/at/rueckgr/kotlin/rocketbot/util/Util.kt
@@ -4,11 +4,22 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
-private const val ZWNBSP = "\uFEFF"
+private const val ZWNBSP = '\uFEFF'
+private const val ZWJ = '\u200D'
 
 fun formatUsername(username: String): String {
+    val offset = findOffsetOfSecondCharacter(username)
     // insert ZWNBSP to avoid highlighting
-    return username.substring(0, 1) + ZWNBSP + username.substring(1)
+    return username.substring(0, offset) + ZWNBSP + username.substring(offset)
+}
+
+fun findOffsetOfSecondCharacter(username: String): Int {
+    for (i in username.indices) {
+        if (!username[i].isSurrogate() && username[i] != ZWJ) {
+            return i
+        }
+    }
+    return username.length
 }
 
 fun toLocalDateTime(zonedDateTime: ZonedDateTime) =
