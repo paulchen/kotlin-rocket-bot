ad3b2ab37db0abdb9a34ba678b3d27598265226c
Paul Staroch <paulchen@rueckgr.at>
2023-01-29 02:26:26 +0100

    properly handle regional indicators

--- src/main/kotlin/at/rueckgr/kotlin/rocketbot/util/Util.kt
+++ src/main/kotlin/at/rueckgr/kotlin/rocketbot/util/Util.kt
@@ -6,6 +6,9 @@ import java.time.ZonedDateTime
 
 private const val ZWNBSP = '\uFEFF'
 private const val ZWJ = '\u200D'
+private const val REGIONAL_INDICATORS_BLOCK = '\uD83C'
+private const val REGIONAL_INDICATORS_START = '\uDDE6'
+private const val REGIONAL_INDICATORS_END = '\uDDFF'
 
 fun formatUsername(username: String): String {
     val offset = findOffsetOfSecondCharacter(username)
@@ -14,9 +17,21 @@ fun formatUsername(username: String): String {
 }
 
 fun findOffsetOfSecondCharacter(username: String): Int {
+    var regionalIndicatorsBlock = false
     for (i in username.indices) {
-        if (!username[i].isSurrogate() && username[i] != ZWJ) {
+        if (!username[i].isSurrogate() && username[i] != ZWJ && !regionalIndicatorsBlock) {
             return i
+        }
+
+        if (i % 2 == 1) {
+            val char1 = username[i-1]
+            val char2 = username[i]
+            if (char1 == REGIONAL_INDICATORS_BLOCK && char2 >= REGIONAL_INDICATORS_START && char2 <= REGIONAL_INDICATORS_END) {
+                regionalIndicatorsBlock = !regionalIndicatorsBlock
+            }
+            else if (regionalIndicatorsBlock) {
+                regionalIndicatorsBlock = false
+            }
         }
     }
     return username.length
