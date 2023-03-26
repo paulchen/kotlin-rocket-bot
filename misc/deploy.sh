e0b77fed95c2a55236c0a5ecd1d76aa8737e0b31
Paul Staroch <paulchen@rueckgr.at>
2023-03-26 23:46:55 +0200

    compare version of dependency to kotlin-rocket-lib after running 'git pull'

--- misc/deploy.sh
+++ misc/deploy.sh
@@ -8,8 +8,6 @@ if [ ! -d ../kotlin-rocket-lib/ ]; then
 	exit 1
 fi
 
-LIB_DEPENDENCY_VERSION=`grep kotlin-rocket-lib bot/build.gradle.kts |sed -e 's/^.*://;s/".*$//'`
-
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
 
 if [ ! -d "$JAVA_HOME" ]; then
@@ -23,16 +21,18 @@ git pull || exit 3
 
 LIB_VERSION=`grep ^version build.gradle.kts |sed -e 's/^[^"]*"//;s/"$//'`
 
-if [ "$LIB_VERSION" != "$LIB_DEPENDENCY_VERSION" ]; then
-	echo "Wrong dependency version $LIB_DEPENDENCY_VERSION for kotlin-rocket-lib (should be $LIB_VERSION)"
-	exit 4
-fi
-
 ./gradlew publishToMavenLocal || exit 3
 
 cd ../kotlin-rocket-bot
 
 git pull || exit 3
+
+LIB_DEPENDENCY_VERSION=`grep kotlin-rocket-lib bot/build.gradle.kts |sed -e 's/^.*://;s/".*$//'`
+
+if [ "$LIB_VERSION" != "$LIB_DEPENDENCY_VERSION" ]; then
+	echo "Wrong dependency version $LIB_DEPENDENCY_VERSION for kotlin-rocket-lib (should be $LIB_VERSION)"
+	exit 4
+fi
 
 docker pull debian:bullseye-slim || exit 3
 docker pull eclipse-temurin:17-jdk || exit 3
