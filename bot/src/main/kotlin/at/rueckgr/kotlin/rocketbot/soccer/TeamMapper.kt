79ae0c3c1f181be091dcd864438327a368587ed7
Paul Staroch <paulchen@rueckgr.at>
2024-06-23 23:32:06 +0200

    fix country names for Scotland and Czech Republic

--- bot/src/main/kotlin/at/rueckgr/kotlin/rocketbot/soccer/TeamMapper.kt
+++ bot/src/main/kotlin/at/rueckgr/kotlin/rocketbot/soccer/TeamMapper.kt
@@ -13,13 +13,17 @@ object TeamMapper {
         "Czech Republic" to ":cz:",
     )
 
+    private val names = mapOf(
+        "Scotland" to "Schottland",
+        "Czech Republic" to "Tschechien",
+    )
+
     fun mapTeamName(teamName: String): FormattedTeamName {
         val locale = LocaleHelper().getLocaleByCountryName(teamName)
         if (locale == null) {
-            if (flags.containsKey(teamName)) {
-                return FormattedTeamName(teamName, flags[teamName]!!)
-            }
-            return FormattedTeamName(teamName, "")
+            val flag = flags[teamName] ?: ""
+            val name = names[teamName] ?: teamName
+            return FormattedTeamName(name, flag)
         }
         val flag = ":flag_" + locale.country.lowercase() + ":"
         return FormattedTeamName(LocaleHelper().getLocalizedCountryName(locale), flag)
