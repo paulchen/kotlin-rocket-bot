5eb218cb166d11c536db95403b85e0c82bd88735
Paul Staroch <paulchen@rueckgr.at>
2024-12-15 02:47:41 +0100

    update dependencies

--- bot/build.gradle.kts
+++ bot/build.gradle.kts
@@ -4,22 +4,22 @@ import java.io.ByteArrayOutputStream
 
 val reflectionsVersion = "0.10.2"
 val coroutinesVersion = "1.9.0"
-val ktorVersion = "3.0.1"
-val jacksonVersion = "2.18.1"
+val ktorVersion = "3.0.2"
+val jacksonVersion = "2.18.2"
 val ktormVersion = "4.1.1"
-val kotlinVersion = "2.0.21"
+val kotlinVersion = "2.1.0"
 val okhttpVersion = "4.12.0"
 
 plugins {
     id("kotlin-rocket-bot.conventions")
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
     application
     groovy
-    id("org.openapi.generator") version "7.9.0"
+    id("org.openapi.generator") version "7.10.0"
     id("com.github.ben-manes.versions") version "0.51.0"
     id("jacoco")
-    id("org.sonarqube") version "5.1.0.4882"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 tasks.named<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask>("dependencyUpdates").configure {
@@ -64,7 +64,7 @@ dependencies {
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
 
     implementation("org.apache.commons:commons-lang3:3.17.0")
-    implementation("org.apache.commons:commons-text:1.12.0")
+    implementation("org.apache.commons:commons-text:1.13.0")
     implementation("org.reflections:reflections:$reflectionsVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
     implementation("at.favre.lib:bcrypt:0.10.2")
@@ -74,17 +74,17 @@ dependencies {
     implementation("org.ktorm:ktorm-core:$ktormVersion")
     implementation("org.ktorm:ktorm-support-postgresql:$ktormVersion")
 
-    implementation("de.focus-shift:jollyday-jaxb:0.34.0")
+    implementation("de.focus-shift:jollyday-jaxb:1.0.0")
 
     // dependencies for generated OpenAPI client
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
-    implementation("com.squareup.moshi:moshi-adapters:1.15.1")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+    implementation("com.squareup.moshi:moshi-adapters:1.15.2")
     implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
     implementation("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test:2.0.21")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.apache.groovy:groovy-all:4.0.23")
+    testImplementation("org.apache.groovy:groovy-all:4.0.24")
     testImplementation("org.spockframework:spock-core:2.4-M4-groovy-4.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
     testImplementation("com.squareup.okhttp3:mockwebserver:$okhttpVersion")
